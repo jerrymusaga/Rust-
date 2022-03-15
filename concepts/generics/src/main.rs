86e841ed201eef4732db02ada6e7a1e3202cfdd6
jerrymusaga <musagatechnology@gmail.com>
2022-03-15 15:17:36 +0100

    more structs

--- concepts/generics/src/main.rs
+++ concepts/generics/src/main.rs
@@ -3,6 +3,28 @@ struct Queue<T> {
     old:Vec<T>
 }
 
+struct Extrema<'elt> {
+    greatest: &'elt i32,
+    least: &'elt i32
+}
+
+fn find_extrema<'s>(slice: &'s [i32]) -> Extrema {
+    let mut greatest = &slice[0];
+    let mut least = &slice[0];
+    for i in 1..slice.len(){
+        if slice[i] < *least {
+            least = & slice[i];
+        }
+        if slice[i] > *greatest {
+            greatest = &slice[i];
+        }
+    }
+    Extrema {
+        greatest,
+        least
+    }
+}
+
 impl<T> Queue<T>{
     fn new() -> Queue<T> {
         Queue {
@@ -25,6 +47,12 @@ impl<T> Queue<T>{
 }
 
 fn main() {
+
+    let z = [-5, 5, 6,4,3];
+    let ex = find_extrema(&z);
+    assert_eq!(*ex.greatest, 6);
+    assert_eq!(*ex.least, -5);
+
     let mut q = Queue::new();
     let mut e = Queue::new();
     q.push(5);
